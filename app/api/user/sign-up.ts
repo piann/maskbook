4fb2ca46b45e10077c0994d3e7cf91c97853df11
Piann IO <shadow_hat@naver.com>
2025-06-01 11:04:43 +0900

    rename sign-up API route

---
+++ app/api/user/sign-up.ts
@@ -0,0 +1,25 @@
+import prisma from "@/lib/server/prisma";
+import { generateSaltedHash } from "@/lib/utils";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest, ctx: any) {
+  const { email, password } = await req.json();
+
+  const passwordHash = generateSaltedHash(password);
+  const post = await prisma.user.create({
+    data: {
+      email,
+      passwordHash,
+    },
+  });
+
+  if (post) {
+    return NextResponse.json({
+      ok: true,
+    });
+  }
+
+  return NextResponse.json({
+    ok: false,
+  });
+}
